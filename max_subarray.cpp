a737edb659ea090cfabf8ae2d14ce1b0c6e2e349
Shovan Kumar Behera <31694748+rockngsho@users.noreply.github.com>
2019-07-03 00:11:14 +0530

    Add files via upload
    Largest Sum Contiguous Sub array

---
+++ max_subarray.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin>>n;
+	vector <int> arr(n);
+	for(int i=0;i<n;i++)
+		cin>>arr[i];
+	
+	int max_so_far=INT_MIN, max_ending_here=0;
+    for(int i=0;i<n;i++)
+    {
+        max_ending_here=max_ending_here+arr[i];
+        if(max_so_far<max_ending_here)
+            max_so_far=max_ending_here;
+            
+        if(max_ending_here<0)
+            max_ending_here=0;
+    }
+    cout<<max_so_far;
+}
